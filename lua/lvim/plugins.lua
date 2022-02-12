0e6c3ede104f61db3b277e693e568c84fde9e2d9
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-02-12 12:14:39 +0100

    fix(ci): avoid calling git-cmd without plenary

--- lua/lvim/plugins.lua
+++ lua/lvim/plugins.lua
@@ -1,4 +1,5 @@
 local commit = {
+  alpha = "8ad63ffb2888a30019d082f08e0b63b9a96ee77c",
   bufferline = "871495d9e2dbe3314a421fd2d5e46f47de7ee537",
   cmp_buffer = "d66c4c2d376e5be99db68d2362cd94d250987525",
   cmp_luasnip = "d6f837f4e8fe48eeae288e638691b91b97d1737f",
@@ -268,6 +269,7 @@ return {
     config = function()
       require("lvim.core.alpha").setup()
     end,
+    commit = "8ad63ffb2888a30019d082f08e0b63b9a96ee77c",
     disable = not lvim.builtin.alpha.active,
   },
 
